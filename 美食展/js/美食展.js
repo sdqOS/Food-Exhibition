6afad566c5c1ff403749a46810fd5998c2bad9df
sdqOS <2088750435@qq.com>
2022-10-23 00:56:30 +0800

    第N次提交

---
+++ 美食展/js/美食展.js
@@ -0,0 +1,108 @@
+	// 披萨
+	var nowimg=1;
+	function pizza(){
+	nowimg=(nowimg>=5)?1:(nowimg+1);
+	document.images["披萨"].src="披萨/"+nowimg+".jpg";
+	}
+    window.setInterval(pizza,1000)
+	// 汉堡包
+	function Hamburger(){
+		nowimg=(nowimg>=5)?1:(nowimg+1);
+		document.images["汉堡包"].src="汉堡包/"+nowimg+".jpg";
+		}
+		window.setInterval(Hamburger,1000)
+	// 薯条
+	function Frenchfries(){
+		nowimg=(nowimg>=5)?1:(nowimg+1);
+		document.images["薯条"].src="薯条/"+nowimg+".jpg";
+		}
+		window.setInterval(Frenchfries,1000)
+	// 千层面
+	function Lasagne(){
+		nowimg=(nowimg>=5)?1:(nowimg+1);
+		document.images["千层面"].src="千层面/"+nowimg+".jpg";
+		}
+		window.setInterval(Lasagne,1000)
+
+
+
+	// 四川火锅
+	function sichuan(){
+		nowimg=(nowimg>=5)?1:(nowimg+1);
+		document.images["四川火锅"].src="四川火锅/"+nowimg+".jpg";
+		}
+		window.setInterval(sichuan,1000)
+	// 兰州牛肉拉面
+	function lanzhou(){
+		nowimg=(nowimg>=5)?1:(nowimg+1);
+		document.images["兰州牛肉拉面"].src="兰州牛肉拉面/"+nowimg+".jpg";
+		}
+		window.setInterval(lanzhou,1000)
+		// 湛江海鲜
+	function zhanjiang(){
+		nowimg=(nowimg>=10)?1:(nowimg+1);
+		document.images["湛江海鲜"].src="湛江海鲜/"+nowimg+".jpg";
+		}
+		window.setInterval(zhanjiang,1000)
+	// 北京烤鸭
+	function beijing(){
+		nowimg=(nowimg>=5)?1:(nowimg+1);
+		document.images["北京烤鸭"].src="北京烤鸭/"+nowimg+".jpg";
+		}
+		window.setInterval(beijing,1000)
+
+	
+
+	// 麻辣烫
+	function malatang(){
+		nowimg=(nowimg>=5)?1:(nowimg+1);
+		document.images["麻辣烫"].src="麻辣烫/"+nowimg+".jpg";
+		}
+		window.setInterval(malatang,500)
+	// 煎饼果子
+	function jianbingguozi(){
+		nowimg=(nowimg>=5)?1:(nowimg+1);
+		document.images["煎饼果子"].src="煎饼果子/"+nowimg+".jpg";
+		}
+		window.setInterval(jianbingguozi,500)
+	// 臭豆腐
+	function choudoufu(){
+		nowimg=(nowimg>=5)?1:(nowimg+1);
+		document.images["臭豆腐"].src="臭豆腐/"+nowimg+".jpg";
+		}
+		window.setInterval(choudoufu,800)
+	// 辣条
+	function latiao(){
+		nowimg=(nowimg>=5)?1:(nowimg+1);
+		document.images["辣条"].src="辣条/"+nowimg+".jpg";
+		}
+		window.setInterval(latiao,666)
+
+
+
+	// 可乐
+	function cola(){
+		nowimg=(nowimg>=5)?1:(nowimg+1);
+		document.images["可乐"].src="可乐/"+nowimg+".jpg";
+		}
+		window.setInterval(cola,800)
+	//果汁
+	function juice(){
+		nowimg=(nowimg>=5)?1:(nowimg+1);
+		document.images["果汁"].src="果汁/"+nowimg+".jpg";
+		}
+		window.setInterval(juice,800)
+	//茶
+	function tea(){
+		nowimg=(nowimg>=5)?1:(nowimg+1);
+		document.images["茶"].src="茶/"+nowimg+".jpg";
+		}
+		window.setInterval(tea,800)
+	//咖啡
+	function Coffee(){
+		nowimg=(nowimg>=5)?1:(nowimg+1);
+		document.images["咖啡"].src="咖啡/"+nowimg+".jpg";
+		}
+		window.setInterval(Coffee,800)
+		
+	
